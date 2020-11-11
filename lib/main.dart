f88ea6f2f2cf2b2a61a57367a8af88cf44cf46c5
Nurul <nurulakter8@gmail.com>
2020-11-11 02:01:18 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NurrLightApp());
+}
+
+class NurrLightApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hellrrerero"),
+    );
+  }
+
+}
